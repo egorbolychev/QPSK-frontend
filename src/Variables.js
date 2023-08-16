8be9dfc4b3a58a25b4eb2c32d4ea088197d4ce1c
Егор Болычев <egorbolycev@MBP-Egor.sknt.ru>
2023-08-16 14:56:37 +0300

    new addr2

--- src/Variables.js
+++ src/Variables.js
@@ -1,5 +1,5 @@
 let API_SERVER_VAL = ''
 let WS_URL_VAL = ''
 // export const WS_SERVER = 'ws' + API_SERVER_VAL.replace("http", "");
-API_SERVER_VAL = "http://server:8000";
+API_SERVER_VAL = "http://localhost:8001";
 export const API_SERVER = API_SERVER_VAL;
